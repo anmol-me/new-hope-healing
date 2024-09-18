b08f3b8a88315612e328e0b6612c68e9d3748e57
Anmol <meani.dev@gmail.com>
2024-09-18 15:55:44 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-sky-400">Paragraph</p>
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
