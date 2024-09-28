629a418d732acde3b4953b4383d7726bfb42dfbf
Anmol <anmol.sin@gmail.com>
2024-09-28 14:41:42 +0530

    Add 4 new reiki services

--- src/Sections/ReikiServices.jsx
+++ src/Sections/ReikiServices.jsx
@@ -2,6 +2,10 @@ import ImageHealing from "../assets/icons/reiki_healing.png";
 import ImageDistance from "../assets/icons/distance.png";
 import ImageRelationship from "../assets/icons/relationship.png";
 import ImageChakras from "../assets/icons/chakras.png";
+import ImageTarot from "../assets/icons/tarot.png";
+import ImageFace from "../assets/icons/face.png";
+import ImagePsychic from "../assets/icons/psychic.png";
+import ImageMeditation from "../assets/icons/meditation.png";
 
 const ReikiServices = () => {
   return (
@@ -82,6 +86,75 @@ const ReikiServices = () => {
                 In a Chakra Balancing session, the practitioner helps align your
                 energy centers. This helps to release blockages, leaving you
                 feeling centered and revitalized.
+              </p>
+            </div>
+          </div>
+
+          {/* Service 5: Tarot Card Reading */}
+          <div className="flex items-center">
+            {/* Image */}
+            <img src={`${ImageTarot}`} alt="Tarot Card Reading" width={100} />
+            {/* Text */}
+            <div className="ml-8">
+              <h3 className="text-2xl font-semibold mb-4">
+                Tarot Card Reading
+              </h3>
+              <p className="text-lg text-gray-700">
+                In a Tarot Reading session, cards are drawn to offer insight
+                into your life, providing guidance and answers to questions
+                about love, career, and personal growth.
+              </p>
+            </div>
+          </div>
+
+          {/* Service 6: Face Reading */}
+          <div className="flex items-center">
+            {/* Image */}
+            <img src={`${ImageFace}`} alt="Face Reading" width={100} />
+            {/* Text */}
+            <div className="ml-8">
+              <h3 className="text-2xl font-semibold mb-4">Face Reading</h3>
+              <p className="text-lg text-gray-700">
+                Face Reading involves the healer connecting with your eyes to
+                reveal unseen aspects of your life, providing profound insights
+                into your emotions, relationships, and spiritual path.
+              </p>
+            </div>
+          </div>
+
+          {/* Service 7: Psychic Reading */}
+          <div className="flex items-center">
+            {/* Image */}
+            <img src={`${ImagePsychic}`} alt="Psychic Reading" width={100} />
+            {/* Text */}
+            <div className="ml-8">
+              <h3 className="text-2xl font-semibold mb-4">Psychic Reading</h3>
+              <p className="text-lg text-gray-700">
+                In a Psychic Reading, the healer taps into intuitive energy to
+                give you guidance on your life path, helping you understand
+                unseen forces and make empowered decisions.
+              </p>
+            </div>
+          </div>
+
+          {/* Service 8: Meditation Sessions */}
+          <div className="flex items-center">
+            {/* Image */}
+            <img
+              src={`${ImageMeditation}`}
+              alt="Meditation Sessions"
+              width={100}
+            />
+            {/* Text */}
+            <div className="ml-8">
+              <h3 className="text-2xl font-semibold mb-4">
+                Meditation Sessions
+              </h3>
+              <p className="text-lg text-gray-700">
+                In a Meditation Session, you’ll be guided through calming
+                exercises to quiet your thoughts, helping you connect with your
+                inner self and restore a sense of balance and calm. This will
+                help you to open up your chakras.
               </p>
             </div>
           </div>
